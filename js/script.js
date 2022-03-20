5013f006f7b5c055908a2e1418bfa2301989236d
Ilya <lord-ilja@yandex.ru>
2022-03-20 17:29:26 +0300

    start of the project

---
+++ js/script.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const tabs = document.querySelectorAll('/tabheader__item'),
+	tabsContent = document.querySelectorAll('.tabcontent'),
+	tabsParent = document.querySelector('.tabheader');
