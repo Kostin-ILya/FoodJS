a7b317d0a5742a1f9e1b72749d3d1f61b359bd7a
Ilya <lord-ilja@yandex.ru>
2022-04-29 11:27:48 +0300

    Calc validation + localStorage

--- js/script.js
+++ js/script.js
@@ -564,11 +564,48 @@ document.addEventListener('DOMContentLoaded', () => {
 	// ============================== Calc
 
 	const result = document.querySelector('.calculating__result span');
-	let sex = 'female',
-		height,
-		weight,
-		age,
+	let sex, height, weight, age, ratio;
+
+	if (localStorage.getItem('sex')) {
+		sex = localStorage.getItem('sex');
+	} else {
+		sex = 'female';
+		localStorage.setItem('sex', sex);
+	}
+	if (localStorage.getItem('ratio')) {
+		ratio = localStorage.getItem('ratio');
+	} else {
 		ratio = '1.375';
+		localStorage.setItem('ratio', ratio);
+	}
+
+	// if (localStorage.getItem('height')) {
+	// 	height = localStorage.getItem('height');
+	// 	document.querySelector('#height').value = height;
+	// }
+	// if (localStorage.getItem('weight')) {
+	// 	weight = localStorage.getItem('weight');
+	// 	document.querySelector('#weight').value = weight;
+	// }
+	// if (localStorage.getItem('age')) {
+	// 	age = localStorage.getItem('age');
+	// 	document.querySelector('#age').value = age;
+	// }
+
+	function initLocalSettings(selector, activeClass) {
+		const elements = document.querySelectorAll(selector);
+
+		elements.forEach((elem) => {
+			elem.classList.remove(activeClass);
+
+			if (elem.getAttribute('id') === localStorage.getItem('sex')) {
+				elem.classList.add(activeClass);
+			}
+			if (elem.getAttribute('data-ratio') === localStorage.getItem('ratio')) {
+				elem.classList.add(activeClass);
+			}
+		});
+	}
 
 	function calcTotal() {
 		if (!sex || !height || !weight || !age || !ratio) {
@@ -587,15 +624,18 @@ document.addEventListener('DOMContentLoaded', () => {
 		}
 	}
 
-	function getStaticInformation(parentSelector, activeClass) {
-		const elements = document.querySelectorAll(`${parentSelector} div`);
+	function getStaticInformation(selector, activeClass) {
+		const elements = document.querySelectorAll(selector);
 
 		elements.forEach((elem) => {
 			elem.addEventListener('click', (e) => {
 				if (e.target.getAttribute('data-ratio')) {
 					ratio = +e.target.getAttribute('data-ratio');
+					localStorage.setItem('ratio', ratio);
+					// localStorage.setItem('ratio', +e.target.getAttribute('data-ratio')); У Ивана так
 				} else {
 					sex = e.target.getAttribute('id');
+					localStorage.setItem('sex', sex);
 				}
 
 				elements.forEach((item) => item.classList.remove(activeClass));
@@ -610,7 +650,24 @@ document.addEventListener('DOMContentLoaded', () => {
 	function getDynamicInformation(selector) {
 		const input = document.querySelector(selector);
 
+		const error = document.createElement('div');
+		error.innerHTML = 'Введите число';
+		error.style.cssText = `
+			color: red;
+			transform: translateX(-30px);    
+			display: flex;
+			align-items: center;    
+		`;
+
 		input.addEventListener('input', () => {
+			if (input.value.match(/\D/g)) {
+				input.style.border = '1px solid red';
+				input.after(error);
+			} else {
+				input.style.border = '';
+				error.remove();
+			}
+
 			switch (input.getAttribute('id')) {
 				case 'height':
 					height = +input.value;
@@ -624,14 +681,34 @@ document.addEventListener('DOMContentLoaded', () => {
 			}
 
 			calcTotal();
+			// switch (input.getAttribute('id')) {
+			// 	case 'height':
+			// 		height = +input.value;
+			// 		localStorage.setItem('height', height);
+			// 		break;
+			// 	case 'weight':
+			// 		weight = +input.value;
+			// 		localStorage.setItem('weight', weight);
+			// 		break;
+			// 	case 'age':
+			// 		age = +input.value;
+			// 		localStorage.setItem('age', age);
+			// 		break;
+			// }  Запись рост/вес/возраст в localStorage, но тогда дефолтные значения перекрывают placeholder и не понятно становится, что это за значения
 		});
 	}
 
+	initLocalSettings('#gender div', 'calculating__choose-item_active');
+	initLocalSettings(
+		'.calculating__choose_big div',
+		'calculating__choose-item_active'
+	);
+
 	calcTotal();
 
-	getStaticInformation('#gender', 'calculating__choose-item_active');
+	getStaticInformation('#gender div', 'calculating__choose-item_active');
 	getStaticInformation(
-		'.calculating__choose_big',
+		'.calculating__choose_big div',
 		'calculating__choose-item_active'
 	);
 
